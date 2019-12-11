232423d112c687b0479941120045eaf43bb4deed
Daniel Zügner <zuegnerd@in.tum.de>
2019-12-11 12:42:33 -0800

    Use collection_prefix keyword to MongoDB observer introduced in PR https://github.com/IDSIA/sacred/pull/704. Enables to also log metrics in a custom collection.

--- seml/database_utils.py
+++ seml/database_utils.py
@@ -232,7 +232,7 @@ def create_mongodb_observer(collection,
     observer = MongoObserver.create(
         url=f'mongodb://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}?authMechanism=SCRAM-SHA-1',
         db_name=db_name,
-        collection=collection,
+        collection_prefix=collection,
         overwrite=overwrite)
 
     return observer
