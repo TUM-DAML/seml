455611d57f37c2f07702b0706fdc44ee92f4cc84
Johannes Klicpera <klicpera@in.tum.de>
2020-02-25 18:45:20 +0100

    Make config file a positional argument

--- seml/main.py
+++ seml/main.py
@@ -235,8 +235,7 @@ if __name__ == '__main__':
                         "See examples/README.md for more details.",
             formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
-            '-c', '--config-file',
-            type=str, required=True,
+            'config_file', type=str,
             help="Path to the YAML configuration file for the experiment.")
     subparsers = parser.add_subparsers(title="Possible operations")
 
