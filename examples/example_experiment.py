97a54e6876df6aec5aedd0507e88b17fbe53992d
Johannes Klicpera <klicpera@in.tum.de>
2020-04-26 13:37:22 +0200

    Fix --debug description and example_experiment output

--- examples/example_experiment.py
+++ examples/example_experiment.py
@@ -27,8 +27,8 @@ def run(dataset, hidden_sizes, learning_rate, reg_scale, keep_prob, max_epochs, 
         regularization_params):
 
     logging.info('Received the following configuration:')
-    logging.info(f'Dataset: {dataset}, hidden sizes: {hidden_sizes}, learning_rate: {learning_rate},'
-                 f'reg_scale: {reg_scale}, keep_prob:{keep_prob}, max_epochs: {max_epochs}, patience:{patience},'
+    logging.info(f'Dataset: {dataset}, hidden sizes: {hidden_sizes}, learning_rate: {learning_rate}, '
+                 f'reg_scale: {reg_scale}, keep_prob:{keep_prob}, max_epochs: {max_epochs}, patience:{patience}, '
                  f'display_step: {display_step}, regularization: {regularization_params}')
 
     #  do your processing here
