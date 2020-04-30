7715e3875125ceca40a013fd1717a727885638c6
Johannes Klicpera <j.klicpera@mailbox.org>
2020-04-30 18:48:41 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='seml',
-    version='0.2.1',
+    version='0.2.2',
     description='Slurm Experiment Management Library',
     long_description=long_description,
     long_description_content_type="text/markdown",
