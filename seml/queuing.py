a37d016288b2a3d1aa23b83c64785dbf2dde30a0
Daniel Zuegner <daniel.zuegner@gmail.com>
2021-03-05 15:52:15 +0100

    Detect duplicates within the config file before queuing to the database.

--- seml/queuing.py
+++ seml/queuing.py
@@ -9,7 +9,7 @@ from seml.utils import s_if, make_hash
 from seml.settings import SETTINGS
 
 
-def filter_experiments(collection, configurations, use_hash=True):
+def filter_experiments(collection, configurations):
     """Check database collection for already present entries.
 
     Check the database collection for experiments that have the same configuration.
@@ -22,8 +22,6 @@ def filter_experiments(collection, configurations, use_hash=True):
         The MongoDB collection containing the experiments.
     configurations: list of dicts
         Contains the individual parameter configurations.
-    use_hash: bool (default: True)
-        Whether to use the hash of the config dictionary to perform a faster duplicate check.
 
     Returns
     -------
@@ -34,8 +32,9 @@ def filter_experiments(collection, configurations, use_hash=True):
 
     filtered_configs = []
     for config in configurations:
-        if use_hash:
-            config_hash = make_hash(config)
+        if 'config_hash' in config:
+            config_hash = config['config_hash']
+            del config['config_hash']
             lookup_result = collection.find_one({'config_hash': config_hash})
         else:
             lookup_dict = {
@@ -109,6 +108,23 @@ def queue_configs(collection, seml_config, slurm_config, configs, source_files=N
 
 
 def queue_experiments(db_collection_name, config_file, force_duplicates, no_hash=False, no_config_check=False):
+    """
+    Queue configurations from a config file into the database.
+
+    Parameters
+    ----------
+    db_collection_name: the MongoDB collection name.
+    config_file: path to the YAML configuration.
+    force_duplicates: if True, disable duplicate detection.
+    no_hash: if True, disable hashing of the configurations for duplicate detection. This is much slower, so use only
+        if you have a good reason to.
+    no_config_check: if True, do not check the config for missing/unused arguments.
+
+    Returns
+    -------
+    None
+    """
+
     seml_config, slurm_config, experiment_config = read_config(config_file)
 
     # Use current Anaconda environment if not specified
@@ -130,8 +146,8 @@ def queue_experiments(db_collection_name, config_file, force_duplicates, no_hash
             slurm_config[k] = v
 
     slurm_config['sbatch_options'] = remove_prepended_dashes(slurm_config['sbatch_options'])
-    collection = get_collection(db_collection_name)
     configs = generate_configs(experiment_config)
+    collection = get_collection(db_collection_name)
 
     batch_id = get_max_in_collection(collection, "batch_id")
     if batch_id is None:
@@ -152,14 +168,36 @@ def queue_experiments(db_collection_name, config_file, force_duplicates, no_hash
     if path is not None:
         git_info = {'path': path, 'commit': commit, 'dirty': dirty}
 
+    use_hash = not no_hash
+    if use_hash:
+        configs = [{**c, **{'config_hash': make_hash(c)}} for c in configs]
+
     if not force_duplicates:
         len_before = len(configs)
-        use_hash = not no_hash
-        configs = filter_experiments(collection, configs, use_hash=use_hash)
+
+        # First, check for duplicates withing the experiment configurations from the file.
+        if not use_hash:
+            # slow duplicate detection without hashes
+            unique_configs = []
+            for c in configs:
+                if c not in unique_configs:
+                    unique_configs.append(c)
+            configs = unique_configs
+        else:
+            # fast duplicate detection using hashing.
+            configs_dict = {c['config_hash']: c for c in configs}
+            configs = [v for k, v in configs_dict.items()]
+
+        len_after_deduplication = len(configs)
+        # Now, check for duplicate configurations in the database.
+        configs = filter_experiments(collection, configs)
         len_after = len(configs)
-        if len_after != len_before:
-            logging.info(f"{len_before - len_after} of {len_before} experiment{s_if(len_before)} were already found "
-                         f"in the database. They were not added again.")
+        if len_after_deduplication != len_before:
+            logging.info(f"{len_before - len_after_deduplication} of {len_before} experiment{s_if(len_before)} were "
+                         f"duplicates. Adding only the {len_after_deduplication} unique configurations.")
+        if len_after != len_after_deduplication:
+            logging.info(f"{len_after_deduplication - len_after} of {len_after_deduplication} "
+                         f"experiment{s_if(len_before)} were already found in the database. They were not added again.")
 
     # Create an index on the config hash. If the index is already present, this simply does nothing.
     collection.create_index("config_hash")
