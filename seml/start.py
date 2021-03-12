d8b23dbfb4e40745d395996f8fba43f6ea7812c5
Daniel Zuegner <daniel.zuegner@gmail.com>
2021-03-12 18:23:12 +0100

    remove unnecessary check for `set_to_pending`.

--- seml/start.py
+++ seml/start.py
@@ -604,11 +604,6 @@ def start_experiments(db_collection_name, local, sacred_id, batch_id, filter_dic
     if filter_dict is None:
         filter_dict = {}
 
-    if use_slurm and not set_to_pending:
-        logging.error("use_slurm is True but set_to_pending is False, which are incompatible. Either perform local"
-                      "processing and set set_to_pending to False, or have set_to_pending=True and use_slurm=True.")
-        return
-
     if unobserved:
         set_to_pending = False
 
