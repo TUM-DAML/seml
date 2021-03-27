fdb55e39d40fed39f2613e816ad2d3cf68a09c3d
Johannes Klicpera <klicpera@in.tum.de>
2021-03-27 14:33:32 +0100

    Errors inherit from SystemExit instead of setting tracebacklimit

--- seml/errors.py
+++ seml/errors.py
@@ -1,29 +1,22 @@
-import sys
-
-
-class ConfigError(Exception):
+class ConfigError(SystemExit):
     """Raised when the something is wrong in the config"""
     def __init__(self, message="The config file contains an error."):
-        sys.tracebacklimit = 0
-        super().__init__(message)
+        super().__init__(f"CONFIG ERROR: {message}")
 
 
-class ExecutableError(Exception):
+class ExecutableError(SystemExit):
     """Raised when the something is wrong with the Python executable"""
     def __init__(self, message="The Python executable has a problem."):
-        sys.tracebacklimit = 0
-        super().__init__(message)
+        super().__init__(f"EXECUTABLE ERROR: {message}")
 
 
-class MongoDBError(Exception):
+class MongoDBError(SystemExit):
     """Raised when the something is wrong with the MongoDB"""
     def __init__(self, message="The MongoDB or its config has a problem."):
-        sys.tracebacklimit = 0
-        super().__init__(message)
+        super().__init__(f"MONGODB ERROR: {message}")
 
 
-class ArgumentError(Exception):
+class ArgumentError(SystemExit):
     """Raised when the something is wrong with the parsed arguments"""
     def __init__(self, message="The parsed arguments contain an error."):
-        sys.tracebacklimit = 0
-        super().__init__(message)
+        super().__init__(f"ARGUMENT ERROR: {message}")
