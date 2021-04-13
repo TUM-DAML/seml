33c1f68c673d166c3282e96181de537c6f141b21
Johannes Klicpera <klicpera@in.tum.de>
2021-04-13 10:34:09 +0200

    Update version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='seml',
-    version='0.3.0',
+    version='0.3.1',
     description='Slurm Experiment Management Library',
     long_description=long_description,
     long_description_content_type="text/markdown",
