3f631e66db61c5ae641552b3c0347902ddd6d3c5
Nicholas Gao <nicholas.gao@tum.de>
2023-07-09 13:05:43 +0200

    respect environment variable to disable rich

--- src/seml/typer.py
+++ src/seml/typer.py
@@ -2,6 +2,9 @@ import os
 
 from seml.module_hider import ModuleHider
 
+if 'SEML_NO_RICH' in os.environ and os.environ['SEML_NO_RICH']:
+    ModuleHider('rich').__enter__()
+
 # If we are in a completion shell, we don't want to import rich.
 # due to its high import time.
 with ModuleHider('rich', hide=bool(os.environ.get('_SEML_COMPLETE'))):
