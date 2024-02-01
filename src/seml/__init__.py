c55fb3f7dedcc49a43f1d7b63e0cf78e5152eff8
Nicholas Gao <nicholas.gao@tum.de>
2024-02-01 11:27:08 +0100

    set version only in one place

--- src/seml/__init__.py
+++ src/seml/__init__.py
@@ -1,5 +1,7 @@
+import importlib.metadata
 from seml.experiment import *  # noqa
 from seml.observers import *  # noqa
 from seml.evaluation import *  # noqa
 
-__version__ = '0.4.0'
+
+__version__ = importlib.metadata.version(__package__ or __name__)
