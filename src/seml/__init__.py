860acb1ec700df80debca5edb00aff3b89d464dd
Nicholas Gao <nicholas.gao@tum.de>
2024-06-21 10:04:34 +0200

    create dummy experiment class during autocompletion

--- src/seml/__init__.py
+++ src/seml/__init__.py
@@ -1,13 +1,12 @@
-import os
-
 from seml.evaluation import *  # noqa
 from seml.experiment import Experiment  # noqa
 from seml.experiment.observers import *  # noqa
+from seml.utils.module_hider import AUTOCOMPLETING
 
 
-if not bool(os.environ.get('_SEML_COMPLETE', False)):
+if AUTOCOMPLETING:
+    __version__ = '0.0.0'
+else:
     import importlib.metadata
 
     __version__ = importlib.metadata.version(__package__ or __name__)
-else:
-    __version__ = '0.0.0'
--- src/seml/experiment/__init__.py
+++ src/seml/experiment/__init__.py
@@ -1,16 +1,1 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from functools import wraps
-
-    from .experiment import Experiment as SemlExperiment
-
-    @wraps(SemlExperiment)
-    def Experiment(*args, **kwargs) -> SemlExperiment:
-        return SemlExperiment(*args, **kwargs)
-else:
-
-    def Experiment(*args, **kwargs):
-        from .experiment import Experiment as SemlExperiment
-
-        return SemlExperiment(*args, **kwargs)
+from .experiment import Experiment  # noqa
