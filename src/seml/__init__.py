85538514e1edd582251ea73925a5d61f0d4a6bd5
Nicholas Gao <nicholas.gao@tum.de>
2024-06-21 09:30:10 +0200

    reorganize imports

--- src/seml/__init__.py
+++ src/seml/__init__.py
@@ -1,23 +1,8 @@
 import os
-from typing import TYPE_CHECKING
 
 from seml.evaluation import *  # noqa
-
-
-if TYPE_CHECKING:
-    from functools import wraps
-
-    from seml.experiment.experiment import Experiment as SemlExperiment
-
-    @wraps(SemlExperiment)
-    def Experiment(*args, **kwargs) -> SemlExperiment:
-        return SemlExperiment(*args, **kwargs)
-else:
-
-    def Experiment(*args, **kwargs):
-        from seml.experiment.experiment import Experiment as SemlExperiment
-
-        return SemlExperiment(*args, **kwargs)
+from seml.experiment import Experiment  # noqa
+from seml.experiment.observers import *  # noqa
 
 
 if not bool(os.environ.get('_SEML_COMPLETE', False)):
