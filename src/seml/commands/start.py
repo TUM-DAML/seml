ecac97d6fb86c7120e53a8dee018e590fefd4b43
Till Grutschus <till.grutschus@tum.de>
2025-11-24 15:14:25 +0100

    reclaiming rescheduled experiments is now preferred; rescheduled experiments may not be claimed by other jobs anymore

--- src/seml/commands/start.py
+++ src/seml/commands/start.py
@@ -1123,6 +1123,7 @@ def claim_experiment(db_collection_name: str, exp_ids: Sequence[int]):
     """
     collection = get_collection(db_collection_name)
     array_id, task_id = get_current_slurm_array_id()
+    exp = None
     if array_id is not None and task_id is not None:
         # We are running in slurm
         array_id, task_id = int(array_id), int(task_id)
@@ -1135,11 +1136,23 @@ def claim_experiment(db_collection_name: str, exp_ids: Sequence[int]):
         exp = collection.find_one_and_update(
             {
                 '_id': {'$in': list(exp_ids)},
-                'status': {'$in': States.PENDING + States.RESCHEDULED},
+                'status': {'$in': States.RESCHEDULED},
+                'execution.array_id': array_id,
+                'execution.task_id': task_id,
+                'execution.cluster': cluster_name,
             },
             {'$set': {'status': States.RUNNING[0], **update}},
             {'_id': 1, 'slurm': 1},
         )
+        if exp is None:
+            exp = collection.find_one_and_update(
+                {
+                    '_id': {'$in': list(exp_ids)},
+                    'status': {'$in': States.PENDING},
+                },
+                {'$set': {'status': States.RUNNING[0], **update}},
+                {'_id': 1, 'slurm': 1},
+            )
         if exp is None:
             exit(3)
         # Set slurm output file
